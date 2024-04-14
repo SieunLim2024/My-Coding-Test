3dcf9f446f42cf0f79fc98bb0779e026271208c3
Sieun Lim <lim2024sieun@gamil.com>
2024-04-14 12:15:24 +0900

    [create] Lv.2 최솟값 만들기

---
+++ CodingTest/src/test240415/Solution.java
@@ -0,0 +1,17 @@
+package test240415;
+
+import java.util.Arrays;
+import java.util.ArrayList;
+class Solution
+{
+    public int solution(int []A, int []B)
+    {
+        int sum=0;
+        Arrays.sort(A);
+        Arrays.sort(B);
+        for(int i=0;i<A.length; i++){
+            sum+= A[i]*B[B.length-i-1];
+        }
+        return sum;
+    }
+}
