382b023e089859ec97e4620797972f71b9ecd4b6
Sieun Lim <lim2024sieun@gmail.com>
2024-04-15 22:49:12 +0900

    [create] Lv.2 JadenCase 문자열 만들기

---
+++ CodingTest/src/test240416/Main.java
@@ -0,0 +1,12 @@
+package test240416;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+		String s= "  for the what 1what  ";
+		System.out.println(sol.solution(s));
+
+	}
+
+}
---
+++ CodingTest/src/test240416/Solution.java
@@ -0,0 +1,21 @@
+package test240416;
+
+class Solution {
+    public String solution(String s) {
+	    s = s.toLowerCase();
+		char[] ch = s.toCharArray();
+		StringBuffer st = new StringBuffer();
+		if (ch[0] >= 97 && ch[0] <= 122) {
+			ch[0] -= 32;
+		}
+		for (int i = 0; i < s.length(); i++) {
+			if (ch[i] == ' ') {
+				if (i<s.length()-1&&ch[i + 1] >= 97 && ch[i + 1] <= 122) {
+					ch[i + 1] -= 32;
+				}
+			}
+			st.append(ch[i]);
+		}
+		return st.toString();
+	}
+}
