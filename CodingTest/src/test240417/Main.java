60771740c685bd8a10941712c198140560980afe
Sieun Lim <lim2024sieun@gmail.com>
2024-04-16 10:03:14 +0900

    [create] Lv.2 이진 변환 반복하기

---
+++ CodingTest/src/test240417/Main.java
@@ -0,0 +1,14 @@
+package test240417;
+
+import java.util.Arrays;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Solution ss = new Solution();
+		String s="110010101001";
+		System.out.println(Arrays.toString(ss.solution(s)));
+
+	}
+
+}
---
+++ CodingTest/src/test240417/Solution.java
@@ -0,0 +1,21 @@
+package test240417;
+
+class Solution {
+    public int[] solution(String s) {
+        int remove=0;
+        int count=0; 
+        boolean flag= false;
+
+        while(!flag){
+            String s2=s.replace("0","");
+            remove+=s.length()-s2.length();
+            count++;
+            s=Integer.toString(s2.length(),2);
+            if(s.equals("1")){
+                flag=true;
+            }
+        }
+        int [] answer ={count,remove};
+        return answer;
+    }
+}
