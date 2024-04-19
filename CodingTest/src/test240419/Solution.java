e7e49861ec0c1bbb62a25327167cd50c8e5cc894
Sieun Lim <lim2024sieun@gmail.com>
2024-04-19 21:52:57 +0900

    [create] Lv.2 짝지어 제거하기

---
+++ CodingTest/src/test240419/Solution.java
@@ -0,0 +1,20 @@
+package test240419;
+
+import java.util.Stack;
+
+class Solution {
+  public int solution(String s) {
+    Stack<Character> stack = new Stack<>();
+    for (int i = 0; i < s.length(); i++) {
+      char c = s.charAt(i);
+      if (!stack.isEmpty() && stack.peek() == c) {
+        stack.pop(); 
+      }
+      else {
+        stack.push(c);
+      }
+    }
+
+    return stack.isEmpty() ? 1 : 0; 
+  }
+}
