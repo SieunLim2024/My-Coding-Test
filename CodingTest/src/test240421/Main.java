6ae4fabfe035ad566b00d211633c1abdf6454a24
Sieun Lim <lim2024sieun@gmail.com>
2024-04-21 23:08:12 +0900

    [create] Lv.1 삼총사

---
+++ CodingTest/src/test240421/Main.java
@@ -0,0 +1,12 @@
+package test240421;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+		int[]a = {1,-1,3,-2,-1};
+		System.out.println(sol.solution(a));
+
+	}
+
+}
