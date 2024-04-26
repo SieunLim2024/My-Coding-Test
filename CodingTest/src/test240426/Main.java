52f879c575b48e6337c0c6aedac29bd8a1c0d7fb
Sieun Lim <lim2024sieun@gmail.com>
2024-04-26 23:15:15 +0900

    [create] Lv2. 점프와 순간 이동

---
+++ CodingTest/src/test240426/Main.java
@@ -0,0 +1,10 @@
+package test240426;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+		System.out.println(sol.solution(5000));
+	}
+
+}
---
+++ CodingTest/src/test240426/Solution.java
@@ -0,0 +1,16 @@
+package test240426;
+
+public class Solution {
+    public int solution(int n) {
+        int ans = 0;
+        while(n>0){
+            if(n%2==1){
+                n--;
+                ans++;
+            }else{
+                n/=2;
+            }
+        }
+        return ans;
+    }
+}
