1a902c54d9071369108c19e1973b68094f89808c
Sieun Lim <lim2024sieun@gmail.com>
2024-04-29 18:54:42 +0900

    [create] Lv.2 귤 고르기

---
+++ CodingTest/src/test240429/Main.java
@@ -0,0 +1,12 @@
+package test240429;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+		int[]a= {1, 3, 2, 5, 4, 5, 2, 3};
+		System.out.println(sol.solution(6, a));
+
+	}
+
+}
---
+++ CodingTest/src/test240429/Solution.java
@@ -0,0 +1,29 @@
+package test240429;
+
+import java.util.*;
+
+class Solution {
+	public int solution(int k, int[] tangerine) {
+		int count = 0;
+		int answer = 0;
+		Arrays.sort(tangerine);
+		ArrayList<Integer> list = new ArrayList();
+
+		for (int i = 0; i < tangerine.length; i++) {
+			count++;
+			if (i != tangerine.length - 1 && tangerine[i] != tangerine[i + 1] || i == tangerine.length - 1) {
+				list.add(count);
+				count = 0;
+			}
+		}
+		Collections.sort(list, Collections.reverseOrder());
+		for (int data : list) {
+			answer++;
+			k -= data;
+			if (k <= 0) {
+				break;
+			}
+		}
+		return answer++;
+	}
+}
