5c35489aa2b3e61bc750a30b0fe4bdf711e219d1
Sieun Lim <lim2024sieun@gmail.com>
2024-05-05 23:52:18 +0900

    [create] Lv.1 옹알이 (2)

---
+++ CodingTest/src/test240505/Main.java
@@ -0,0 +1,12 @@
+package test240505;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+		String[] a= {"aya", "yee", "u", "maa"};
+		System.out.println(sol.solution(a));
+
+	}
+
+}
---
+++ CodingTest/src/test240505/Solution.java
@@ -0,0 +1,24 @@
+package test240505;
+
+class Solution {
+	public int solution(String[] babbling) {
+		String[] able = { "aya", "ye", "woo", "ma" };
+		for (int i = 0; i < babbling.length; i++) {
+			for (int j = 0; j < able.length; j++) {
+				if (babbling[i].indexOf(able[j] + able[j]) != -1) {
+					break;
+				} else if (babbling[i].indexOf(able[j]) != -1) {
+					babbling[i] = babbling[i].replace(able[j], "");
+					j--;
+				}
+			}
+		}
+		int answer = 0;
+        for (int i = 0; i < babbling.length; i++) {
+            if(babbling[i].equals("")){
+                answer++;
+            }
+        }
+		return answer;
+	}
+}
