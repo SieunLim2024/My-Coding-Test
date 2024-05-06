1276603da02cf6459a16858417408370887749a8
Sieun Lim <lim2024sieun@gmail.com>
2024-05-06 18:56:16 +0900

    [fix] Lv.1 옹알이 (2) Resolves: 연속되지 않는 동일한 발음이 있는 단어도 카운트 가능하게 수정.

--- CodingTest/src/test240505/Solution.java
+++ CodingTest/src/test240505/Solution.java
@@ -8,14 +8,14 @@ class Solution {
 				if (babbling[i].indexOf(able[j] + able[j]) != -1) {
 					break;
 				} else if (babbling[i].indexOf(able[j]) != -1) {
-					babbling[i] = babbling[i].replace(able[j], "");
+					babbling[i] = babbling[i].replace(able[j], " ");
 					j--;
 				}
 			}
 		}
 		int answer = 0;
         for (int i = 0; i < babbling.length; i++) {
-            if(babbling[i].equals("")){
+        	if(babbling[i].trim().length() == 0){
                 answer++;
             }
         }
