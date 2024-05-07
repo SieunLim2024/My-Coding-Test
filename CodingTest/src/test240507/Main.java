08d6647f8913a5ee0d56c6ee404c7a8736b28b75
Sieun Lim <lim2024sieun@gmail.com>
2024-05-07 22:02:32 +0900

    [create] Lv.1 예산

---
+++ CodingTest/src/test240507/Main.java
@@ -0,0 +1,9 @@
+package test240507;
+
+public class Main {
+	public static void main(String args[]) {
+		Solution sol = new Solution();
+		int[] d= {1,3,2,5,4};
+		System.out.println(sol.solution(d, 9));
+	}
+}
