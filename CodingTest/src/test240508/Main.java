566ec875de0cb4e6328d5920474b8e6fef23424f
Sieun Lim <lim2024sieun@gmail.com>
2024-05-08 23:25:09 +0900

    [create] Lv.1 과일 장수

---
+++ CodingTest/src/test240508/Main.java
@@ -0,0 +1,12 @@
+package test240508;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+		int[] a= {1, 2, 3, 1, 2, 3, 1};
+		System.out.println(sol.solution(3,4,a));
+
+	}
+
+}
