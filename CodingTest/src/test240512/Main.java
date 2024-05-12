3cc3c4d95e5e6dd5bfbedd7c802c1ba3a541b1b8
Sieun Lim <lim2024sieun@gmail.com>
2024-05-12 21:16:34 +0900

    [create] Lv.1 문자열 나누기

---
+++ CodingTest/src/test240512/Main.java
@@ -0,0 +1,11 @@
+package test240512;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+		String s ="banana";
+		System.out.println(sol.solution(s));
+	}
+
+}
