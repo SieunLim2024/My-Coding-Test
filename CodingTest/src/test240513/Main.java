9514e5ec972b9bcbd9c02c74912b261f49658069
Sieun Lim <lim2024sieun@gmail.com>
2024-05-13 23:28:16 +0900

    [create] Lv.1 햄버거 만들기

---
+++ CodingTest/src/test240513/Main.java
@@ -0,0 +1,9 @@
+package test240513;
+
+public class Main {
+	public static void main (String arg[]) {
+		Solution sol = new Solution();
+		int[] a= {2, 1, 1, 2, 3, 1, 2, 3, 1};
+		System.out.println(sol.solution(a));
+	}
+}
