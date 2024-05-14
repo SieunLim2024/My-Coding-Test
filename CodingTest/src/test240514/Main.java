295be1e7b0cd61faa5f96b342ed33bdd8095daf7
Sieun Lim <lim2024sieun@gmail.com>
2024-05-14 23:22:22 +0900

    [create] Lv.1 가장 가까운 같은 글자

---
+++ CodingTest/src/test240514/Main.java
@@ -0,0 +1,13 @@
+package test240514;
+
+import java.util.Arrays;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+		String s="banana";
+		System.out.println(Arrays.toString(sol.solution(s)));
+	}
+
+}
