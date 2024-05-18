f3899f32c30c7a4e19142275165a5fd0f9cd8d83
Sieun Lim <lim2024sieun@gmail.com>
2024-05-18 23:32:35 +0900

    [create] Lv.1 최소직사각형

---
+++ CodingTest/src/test240518/Main.java
@@ -0,0 +1,11 @@
+package test240518;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+		int[][] a = {{60, 50}, {30, 70}, {60, 30}, {80, 40}};
+		System.out.println(sol.solution(a));
+	}
+
+}
