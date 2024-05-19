569f9918da1a40dc81d75afe44ce44f3438603f7
Sieun Lim <lim2024sieun@gmail.com>
2024-05-19 22:35:37 +0900

    [create] Lv.2 2018 KAKAO BLIND RECRUITMENT [1차] 캐시

---
+++ CodingTest/src/test240519/Main.java
@@ -0,0 +1,10 @@
+package test240519;
+
+public class Main {
+	public static void main(String ars[]) {
+		Solution sol =new Solution();
+		String[] a= {"Jeju", "Pangyo", "Seoul", "NewYork", "LA", "SanFrancisco", "Seoul", "Rome", "Paris", "Jeju", "NewYork", "Rome"};
+		System.out.println(sol.solution(3, a));
+	}
+	
+}
