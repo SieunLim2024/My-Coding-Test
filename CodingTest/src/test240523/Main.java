bde432d3ecf1da5d36645d740bcb09210ffdea80
Sieun Lim <lim2024sieun@gmail.com>
2024-05-23 23:35:35 +0900

    [create] Lv.2 Summer/Winter Coding(~2018) 영어 끝말잇기

---
+++ CodingTest/src/test240523/Main.java
@@ -0,0 +1,13 @@
+package test240523;
+
+import java.util.Arrays;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+		String[] a= {"tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"};
+		System.out.println(Arrays.toString(sol.solution(3, a)));
+	}
+
+}
