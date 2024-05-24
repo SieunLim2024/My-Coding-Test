9260ed549c3f02927e316413077244d08ac10f32
Sieun Lim <lim2024sieun@gmail.com>
2024-05-24 23:56:43 +0900

    [create] Lv.1 수박수박수박수박수박수?

---
+++ CodingTest/src/test240524/Main.java
@@ -0,0 +1,10 @@
+package test240524;
+
+import java.util.Arrays;
+
+public class Main {
+	public static void main(String args[]) {
+		Solution sol = new Solution();
+		System.out.println(sol.solution(3));
+	}
+}
