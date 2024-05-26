3d79bcd409cc200fe55071f3614839c2d25ffdf3
Sieun Lim <lim2024sieun@gmail.com>
2024-05-26 23:45:30 +0900

    [create] Lv.1 핸드폰 번호 가리기

---
+++ CodingTest/src/test240526/Main.java
@@ -0,0 +1,11 @@
+package test240526;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+		System.out.println(sol.solution("010123412345"));
+
+	}
+
+}
