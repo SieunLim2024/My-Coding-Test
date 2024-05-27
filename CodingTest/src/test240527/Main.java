75ec3840d77785679991c497cf41a8cfe6448357
Sieun Lim <lim2024sieun@gmail.com>
2024-05-27 22:18:23 +0900

    [create] Lv.2 롤케이크 자르기

---
+++ CodingTest/src/test240527/Main.java
@@ -0,0 +1,12 @@
+package test240527;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+		int[] t = {1, 2, 1, 3, 1, 4, 1, 2};
+		System.out.println(sol.solution(t));
+
+	}
+
+}
