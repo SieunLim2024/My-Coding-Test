f64089c2852ae42eb79d0c7b55b862d9ef3e2177
Sieun Lim <lim2024sieun@gmail.com>
2024-05-31 22:13:44 +0900

    [create] Lv.1 문자열 내 마음대로 정렬하기

---
+++ CodingTest/src/test240531/Main.java
@@ -0,0 +1,14 @@
+package test240531;
+
+import java.util.Arrays;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Solution sol =new Solution();
+		String[] a= {"abce", "abcd", "cdx"};
+		System.out.println(Arrays.toString(sol.solution(a,2)));
+
+	}
+
+}
