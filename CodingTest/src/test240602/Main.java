52a99faed15017548dce2a36db39d2a11946a9d4
Sieun Lim <lim2024sieun@gmail.com>
2024-06-02 22:39:28 +0900

    [create] Lv.1 폰켓몬

---
+++ CodingTest/src/test240602/Main.java
@@ -0,0 +1,9 @@
+package test240602;
+
+public class Main {
+	public static void main(String args[]) {
+		Solution sol = new Solution();
+		int[] n = {3,3,3,2,2,4};
+		System.out.println(sol.solution(n));
+	}
+}
