81c8ce5fec5dbf52bfe09e7fb068f746b845cb36
Sieun Lim <lim2024sieun@gmail.com>
2024-06-04 23:04:57 +0900

    [create] Lv.1 명예의 전당 (1)

---
+++ CodingTest/src/test240604/Main.java
@@ -0,0 +1,14 @@
+package test240604;
+
+import java.util.Arrays;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+		int[] a= {10, 100, 20, 150, 1, 100, 200};
+		System.out.println(Arrays.toString(sol.solution(3, a)));
+
+	}
+
+}
