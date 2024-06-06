8674c706d4e849606cf3fcb8d3df1d1b323c9bff
Sieun Lim <lim2024sieun@gmail.com>
2024-06-06 23:45:28 +0900

    [create] Lv.2 튜플

---
+++ CodingTest/src/test240605and0606/Main.java
@@ -0,0 +1,11 @@
+package test240605and0606;
+
+import java.util.Arrays;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+		System.out.println(Arrays.toString(sol.solution("{{2},{2,1},{2,1,3},{2,1,3,4}}")));
+	}
+}
