91dfefadf05e212805ca932d289dac1cd67177bc
Sieun Lim <lim2024sieun@gmail.com>
2024-06-07 23:28:11 +0900

    [create] Lv.2 n^2 배열 자르기

---
+++ CodingTest/src/test240607/Main.java
@@ -0,0 +1,13 @@
+package test240607;
+
+import java.util.Arrays;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+		System.out.println(Arrays.toString(sol.solution(3, 2, 5)));
+
+	}
+
+}
