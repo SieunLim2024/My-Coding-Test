cd4641aefbebcec4927702cc9d951f8404951b60
Sieun Lim <lim2024sieun@gmail.com>
2024-06-08 23:47:54 +0900

    [create] Lv.2 프로세스

---
+++ CodingTest/src/test240608/Main.java
@@ -0,0 +1,10 @@
+package test240608;
+
+public class Main {
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+		int[] a= {2, 1, 3, 2};
+		System.out.println(sol.solution(a, 2));
+	}
+
+}
